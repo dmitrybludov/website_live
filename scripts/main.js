17f057b2993eaadf7a91492031e292a30d24a843
Dmitry <bludovdmitry@gmail.com>
2019-09-02 00:54:32 +0300

    Creating the Responsive Menu

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,20 @@ $(document).ready(function(){
 		$("body").toggleClass("scroll", (scrollFromTop > 350));
 	})
 
+	// SMOOTH SCROLL
+
+	$('.menu li a[href^="#"]').on('click', function(e){
+		e.preventDefault();
+
+		var target = $(this.hash);
+
+		if (target.length) {
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top -60
+			}, 1000);
+		}
+	});
+
 	// MASONRY
 
 	$('.grid').masonry({
@@ -27,6 +41,24 @@ $(document).ready(function(){
 		slidesToShow: 3,
 		prevArrow:'<button type="button" class="slick-prev">Previous</button>',
 		nextArrow:'<button type="button" class="slick-next">Next</button>'
+	});
+
+	// RESPONSIVE MENU START
+
+	var body = $('body');
+	var menuTrigger = $('.js-menu-trigger');
+	var mainOverlay = $('.js-main-overlay');
+
+	menuTrigger.on('click', function() {
+		body.addClass('menu-is-active');
+	});
+
+	mainOverlay.on('click', function() {
+		body.removeClass('menu-is-active');
+	});
+
+	$('.menu li a').on('click', function() {
+		$('body').removeClass('menu-is-active');
 	});
 
 });
