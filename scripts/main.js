b861fb614f243554cc37ce0ccdbf5bde358e2e07
Dmitry <bludovdmitry@gmail.com>
2019-09-02 16:27:06 +0300

    Developing the Website for Laptop & Mobile Use

--- scripts/main.js
+++ scripts/main.js
@@ -40,7 +40,20 @@ $(document).ready(function(){
 		centerMode: true,
 		slidesToShow: 3,
 		prevArrow:'<button type="button" class="slick-prev">Previous</button>',
-		nextArrow:'<button type="button" class="slick-next">Next</button>'
+		nextArrow:'<button type="button" class="slick-next">Next</button>',
+
+		responsive: [{
+			breakpoint: 990,
+			settings: {
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1
+			}
+		}]
 	});
 
 	// RESPONSIVE MENU START
